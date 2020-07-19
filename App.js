be62a4713621acc105dae1f9cea3c7a1c89cca29
Edmund Xin <mxinburritos@gmail.com>
2020-07-19 10:25:37 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Mark Edmund Xin</Text>
+      <Button title='My First Button' icon={{ name: 'code' }} />
+      <StatusBar style='auto' />
     </View>
   );
 }
